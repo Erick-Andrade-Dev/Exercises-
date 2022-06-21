3fca278245caee91c00460bc9f64fe05db7ae27d
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-06-21 19:11:51 -0300

    Introdução_JavaScript

---
+++ estudo.js
@@ -0,0 +1,34 @@
+const valor1 = 5;
+const valor2 = 10;
+
+let soma = valor1 + valor2; 
+console.log(soma);
+
+let subtração = valor1 - valor2;
+console.log(subtração);
+
+let multiplicação = valor1 * valor2;
+console.log(multiplicação);
+
+let divisão = valor1 / valor2;
+console.log(divisão);
+
+let modulo = valor1 % valor2;
+console.log(modulo);
+
+let x = 100;
+
+if (x > 100) {
+    console.log("Wins");
+} 
+
+else if(x == 100) { 
+    console.log("Equal")
+}
+
+else  { 
+    console.log("Lose");
+}
+
+
+
