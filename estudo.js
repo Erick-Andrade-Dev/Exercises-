ebfa44239d9c2e3506080d554ae2be3cc573d207
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-06-24 11:14:13 -0300

    JS_Arrays/for

--- estudo.js
+++ estudo.js
@@ -109,6 +109,37 @@ console.log("Menu de acompanhamentos: ", cardapio);
 console.log(cardapioCarne);
 console.log(indexOfCarne);
 
+let academia = ['Creatina','Whey Protein', "BCA"];
+
+for (let index = 0; index < academia.length; index +=1) {
+    let messagem = "Pré Treino bateuuu!"  + academia[index] + "!";
+    console.log(messagem,);
+}
+
+let numbers = [9, 7, 5, 10, 15];
+
+numbers.sort((a, b) => a - b) ;
+console.log(numbers);
+
+let somar = (9 + 7 + 5 + 10 + 15)
+console.log(somar)
+
+let dividir = (somar / 5);
+console.log(dividir)
+
+if (dividir > 25) {
+    console.log("maior que 25")
+} else{
+    console.log("menor que 25")
+}
+
+for (let n = 0; n < numbers.length; n =+1); {
+    console.log(n)
+}
+
+
+
+
 
 
 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,20 @@
+let numbers = [9, 7, 5, 10, 15];
+
+numbers.sort((a, b) => a - b) ;
+console.log(numbers);
+
+let somar = (9 + 7 + 5 + 10 + 15)
+console.log(somar)
+
+let dividir = (somar / 5);
+console.log(dividir)
+
+if (dividir > 25) {
+    console.log("maior que 25")
+} else{
+    console.log("menor que 25")
+}
+
+for (let n = 0; n < numbers.length; n =+1); {
+    console.log(n)
+}
