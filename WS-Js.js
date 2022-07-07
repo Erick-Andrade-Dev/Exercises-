c904dfe02326f29b2d226e0399b480c56b0d72bd
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-07-07 19:14:10 -0300

    Web-storage-JS-Exercises

--- WS-Js.js
+++ WS-Js.js
@@ -27,17 +27,44 @@ window.onload = function () {
         localStorage.setItem('colorFont', selectFont.value );
         
     })
+    let styleText = document.querySelector('#fontStyle')    
+    styleText.addEventListener('change', function(){ 
+        let styleP = styleText.selectedOptions
+        let containerP = document.querySelector('p')
+        containerP.style.fontFamily = styleText.value
+
+        localStorage.setItem('style-font', styleText.value)
+
+    })
+    let spaceText = document.querySelector('input[name=font-space')   
+    spaceText.addEventListener('change', function(){
+        let storeparagraph = document.querySelector('p');
+        storeparagraph.style.margin = `${spaceText.value}px`;
+
+        localStorage.setItem('space-font', `${spaceText.value}px`)
+        
+    })
+
+    
     const fontpx = localStorage.getItem('font-sizepx');
    
     const textP = document.querySelector('p');
-    textP.style.fontSize = fontpx     
+    textP.style.fontSize = fontpx;
     /////////////////////////////////////////////////////////////
     const corText = localStorage.getItem('colorFont');
 
     const colorText = document.querySelector('p')
-    colorText.style.color = corText
+    colorText.style.color = corText;
+    ////////////////////////////////////////////////////////////
+    const styleParagraph = localStorage.getItem('style-font')
     
-    
+    const fontParagraph = document.querySelector('p')
+    fontParagraph.style.fontFamily = styleParagraph;
+    ///////////////////////////////////////////////////////
+    const spaceParagrafh = localStorage.getItem('space-font')
+
+    const marginText= document.querySelector('p')
+    marginText.style.margin = spaceParagrafh; 
 }
 
 ////////////////////////////////
